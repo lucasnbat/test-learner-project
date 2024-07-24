5817e7131b83f2e6b3cff85af0c68a791c7288ca
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-24 17:40:49 -0400

    initial: server mínimo

---
+++ src/database.ts
@@ -0,0 +1,20 @@
+/**
+ * Configuração de database
+ */
+
+import { knex as setupKnex, Knex } from 'knex';
+
+export const config: Knex.Config = {
+    client: 'sqlite3',
+    connection: {
+        filename: './db/app.db'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations',
+    }
+}
+
+// constante contendoo setup do Knex com as configurações
+export const knex = setupKnex(config)
