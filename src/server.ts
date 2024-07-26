78b530e9f535d7da24014f817cceaa621a41e42b
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-26 16:24:03 -0400

    adiciona modif. para ouvir todos os hosts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { env } from './env'
 
 app
     .listen({
-        port: env?.PORT,
+        port: env?.PORT || 3333,
+        host: '0.0.0.0'
     })
     .then(() => {
         console.log('Server running on port 3333')
