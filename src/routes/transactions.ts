0939aa8d16a69c9b12cab1947fb6067d0b96563a
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-25 23:24:16 -0400

    adiciona preHandler/middleware de autenticação por meio do session_id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,63 +2,105 @@ import { FastifyInstance } from "fastify";
 import { knex } from "../database";
 import { z } from "zod";
 import { randomUUID } from "crypto";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 // isso é um plugin do fastify, sempre deve ser function assincrona (async)
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/', async () => {
-        const transactions = await knex('transactions').select()
+    app.get(
+        '/',
+        {
+            preHandler: [checkSessionIdExists]
+        },
+        async (request) => {
 
-        return { transactions };
-    })
+            const { sessionId } = request.cookies;
 
-    app.get('/:id', async (request) => {
-        const getTransactionParamsSchema = z.object({
-            id: z.string().uuid(),
-        })
-        const { id } = getTransactionParamsSchema.parse(request.params)
+            const transactions = await knex('transactions')
+                .where('session_id', sessionId)
+                .select()
 
-        const transaction = await knex('transactions').where('id', id).first();
-
-        return { transaction }
-    })
-
-    app.get('/summary', async () => {
-        const summary = await knex('transactions')
-            .sum('amount', { as: 'amount' })
-            .first()
-
-        return { summary }
-    })
-
-    app.post('/', async (request, reply) => {
-
-        const createTransactionBodySchema = z.object({
-            title: z.string(),
-            amount: z.number(),
-            type: z.enum(['credit', 'debit']),
+            return { transactions };
         })
 
-        const { title, amount, type } = createTransactionBodySchema.parse(request.body);
-
-        let sessionId = request.cookies.sessionId;
-
-        if (!sessionId) {
-            sessionId = randomUUID()
-
-            reply.cookie('sessionId', sessionId, {
-                path: '/', // quais rotas podem acessar? / = todas
-                maxAge: 60 * 60 * 24 * 7 // 7 days
+    app.get(
+        '/:id',
+        {
+            preHandler: [checkSessionIdExists]
+        },
+        async (request) => {
+            const getTransactionParamsSchema = z.object({
+                id: z.string().uuid(),
             })
-        }
+            const { id } = getTransactionParamsSchema.parse(request.params)
 
-        await knex('transactions').insert({
-            id: crypto.randomUUID(),
-            title,
-            amount: type === 'credit' ? amount : amount * -1,
-            session_id: sessionId,
+            const { sessionId } = request.cookies
+
+            const transaction = await knex('transactions')
+                .where({
+                    session_id: sessionId,
+                    id,
+                })
+                .first();
+
+            return { transaction }
         })
 
-        return reply.status(201).send();
-    })
+    app.get(
+        '/summary',
+        {
+            preHandler: [checkSessionIdExists]
+        },
+        async (request) => {
+            const { sessionId } = request.cookies;
+
+            const summary = await knex('transactions')
+                .where('session_id', sessionId)
+                .sum('amount', { as: 'amount' })
+                .first()
+
+            return { summary }
+        })
+
+    app.post(
+        '/',
+        {
+            preHandler: [checkSessionIdExists]
+        },
+        async (request, reply) => {
+
+            const createTransactionBodySchema = z.object({
+                title: z.string(),
+                amount: z.number(),
+                type: z.enum(['credit', 'debit']),
+            })
+
+            // destructuring para pegar os valores resultantes do parse
+            const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+
+            // adiciona dinamicamente uma chave com o cookie
+            let sessionId = request.cookies.sessionId;
+
+            if (!sessionId) {
+
+                // cria sessionId
+                sessionId = randomUUID()
+
+                // adiciona o sessionId como info. de cookie guardada na resposta (reply, response)
+                reply.cookie('sessionId', sessionId, {
+                    path: '/', // quais rotas podem acessar? / = todas
+                    maxAge: 60 * 60 * 24 * 7 // 7 days
+                })
+            }
+
+            // insere dados no banco
+            await knex('transactions').insert({
+                id: crypto.randomUUID(),
+                title,
+                amount: type === 'credit' ? amount : amount * -1,
+                session_id: sessionId,
+            })
+
+            return reply.status(201).send();
+        })
 }
 
