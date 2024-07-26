16e4e72ebaef75e2b6588a7aedd80e3459625216
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-07-26 13:43:45 -0400

    adiciona teste de summary

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -26,25 +26,18 @@ describe('Transactions routes', () => {
         await request(app.server) // cria uma requisição acessando o servidor nativo do node:http
             .post('/transactions') // faz um post em /transactions
             .send({ // envia os dados
-                title: 'Test transaction from Vitest',
+                title: 'Test transaction',
                 amount: 5000,
                 type: 'credit'
             })
             .expect(201) // e aguarda retorno de status 201
     })
 
-    /* 
-     * para listar transações precisa criar uma.
-     * você NÃO PODE usar o resultado do teste de criar transaction. 
-     * então você deve CRIAR uma transaction antes de listar
-     * escreva os teste como se só ele existisse
-     * ele deve ser ISOLADO de outros contextos
-     */
     it('should be able to list all transactions', async () => {
         const createTransactionResponse = await request(app.server) // cria uma requisição acessando o servidor nativo do node:http
             .post('/transactions') // faz um post em /transactions
             .send({ // envia os dados
-                title: 'Test transaction from Vitest',
+                title: 'Test transaction',
                 amount: 5000,
                 type: 'credit'
             })
@@ -58,12 +51,85 @@ describe('Transactions routes', () => {
 
         expect(listTransactionsResponse.body.transactions).toEqual([
             expect.objectContaining({
-                title: 'Test transaction from Vitest',
+                title: 'Test transaction',
                 amount: 5000,
             })
         ])
     })
+
+    it('should be able to list specific transaction', async () => {
+        const createTransactionResponse = await request(app.server) // cria uma requisição acessando o servidor nativo do node:http
+            .post('/transactions') // faz um post em /transactions
+            .send({ // envia os dados
+                title: 'Test transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', String(cookies))
+            .expect(200)
+
+        // implementação para pegar transaction específica
+
+        // pegando primeira transaction da lista para exemplo
+        const transactionId = listTransactionsResponse.body.transactions[0].id
+
+        const getTransactionResponse = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', String(cookies))
+            .expect(200)
+
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'Test transaction',
+                amount: 5000
+            })
+        )
+    })
+
+    it('should be able to get the summary', async () => {
+        const createTransactionResponse = await request(app.server) // cria uma requisição acessando o servidor nativo do node:http
+            .post('/transactions') // faz um post em /transactions
+            .send({ // envia os dados
+                title: 'Credit transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        await request(app.server) // cria uma requisição acessando o servidor nativo do node:http
+            .post('/transactions') // faz um post em /transactions
+            .set('Cookie', String(cookies))
+            .send({ // envia os dados
+                title: 'Debit transaction',
+                amount: 2000,
+                type: 'debit'
+            })
+
+        const summaryResponse = await request(app.server)
+            .get('/transactions/summary')
+            .set('Cookie', String(cookies))
+            .expect(200)
+
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 3000
+        })
+    })
 })
+
+
+/*
+ * para listar transações precisa criar uma.
+ * você NÃO PODE usar o resultado do teste de criar transaction. 
+ * então você deve CRIAR uma transaction antes de listar
+ * escreva os teste como se só ele existisse
+ * ele deve ser ISOLADO de outros contextos
+ */
 
 /**
  * it.only: executa apenas esse teste
